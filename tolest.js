44604b8e801aa9edf089cff414e6785f827287e1
MD Enjamul Islam <71033689+mdenjamulislam@users.noreply.github.com>
2023-08-17 14:58:56 +0000

    Function declaration and solving

--- tolest.js
+++ tolest.js
@@ -11,3 +11,20 @@ Print the height of the tallest friend (cm).
 
 
 
+function maxInArray(numbers) {
+    let largest = numbers[0];
+    for(let i = 0; i < numbers.length; i++) {
+        const index = i;
+        const element = numbers[index];
+        console.log(element);
+        if (element > largest) {
+            largest = element;
+            
+        }
+    }
+    return largest;
+}
+
+const heights = [167, 192, 120, 165, 137];
+const tallest = maxInArray(heights);
+console.log("Tallest person is: ",tallest,'cm');
