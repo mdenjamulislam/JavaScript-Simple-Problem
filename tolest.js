510eb0f74f8e568aab3ac1d6481cca9d62193cb2
MD Enjamul Islam <71033689+mdenjamulislam@users.noreply.github.com>
2023-08-17 14:58:05 +0000

    Problem init

---
+++ tolest.js
@@ -0,0 +1,13 @@
+/*
+
+Tom and his friends are participating in the 'Who is the tallest competition. As per the name, the person with the highest height will be the winner. Can you find who is the tallest among Tom and all of his friends?
+
+Input: 
+The input line can have multiple integer numbers, xi(The height of ith friend in cm).
+Output:
+Print the height of the tallest friend (cm).
+
+*/
+
+
+
