f05b3464fbd8776543e4b722b31eca377a9f550d
MD Enjamul Islam <71033689+mdenjamulislam@users.noreply.github.com>
2023-08-19 06:38:19 +0000

    Discount problem init

---
+++ discount.js
@@ -0,0 +1,11 @@
+/*
+
+1. If tickrt numbers is less than 100, per ticket price 100;
+2. If ticket number is more than 100, but less than 200, First 100 tickets will be 100/perticket.
+RWST ticket will be 90 taks per piece. 
+3. if you purchase more than 200 tickets 
+    first 100 ----> 100tk
+        101 - 200 ----> 90tk
+        200+ ----> 70tk
+
+*/
