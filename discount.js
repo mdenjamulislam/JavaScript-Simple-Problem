f95abdf58495401008fc731684cc43ba92822e6f
MD Enjamul Islam <71033689+mdenjamulislam@users.noreply.github.com>
2023-08-19 07:58:46 +0000

    Finished dicsount calculator

--- discount.js
+++ discount.js
@@ -9,3 +9,31 @@ RWST ticket will be 90 taks per piece.
         200+ ----> 70tk
 
 */
+
+function ticketPrice(ticketQuentity) {
+    const first100Rate = 100;
+    const second100Rate= 90;
+    const restTiccketRate = 70;
+
+    if(ticketQuentity <= 100) {
+        const price = ticketQuentity * first100Rate;
+        return price;
+    }else if(ticketQuentity <= 200) {
+        const first100Price = 100 * first100Rate;
+        const restTicketQuestity = ticketQuentity - 100;
+        const restTicketPrice = restTicketQuestity * second100Rate;
+        const totalPrice = first100Price + restTicketPrice;
+        return totalPrice;
+    }else {
+        const first100Price = 100 * first100Rate;
+        const second100Price = 100 * second100Rate;
+        const restTicketQuentity = ticketQuentity - 200;
+        const restTicketPrice = restTicketQuentity * restTiccketRate;
+        const totalPrice = first100Price + second100Price + restTicketPrice;
+        return totalPrice;
+    }
+    
+}
+
+const price = ticketPrice(109);
+console.log(price);
